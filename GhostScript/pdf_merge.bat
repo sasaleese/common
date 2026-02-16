f48acd2e495cceb395f34b0bc4896e51532cf90b
sasaleese <igloigabor@gmail.com>
2026-02-16 22:14:45 +0100

    Update pdf_merge.bat

--- GhostScript/pdf_merge.bat
+++ GhostScript/pdf_merge.bat
@@ -9,10 +9,10 @@ REM --- Kimeneti fájl üritése ---
 type nul > "%OUT%"
 
 REM --- ABC szerinti stabil sorrend ---
-for /f "delims=" %%F in ('dir /b /on *.pdf *.PDF') do (
+for /f "delims=" %%F in ('dir /b /on *.pdf') do (
     echo %%~nxF
     echo ===== [%%~nxF] =====>> "%OUT%"
-    "%PDFTOTEXT%" -dBATCH -dNOPAUSE -q -sDEVICE=txtwrite -sOutputFile=- "%%~fF" >> "%OUT%" 2>nul
+    "%PDFTOTEXT%" -dBATCH -dNOPAUSE -sDEVICE=txtwrite -q -sOutputFile=- "%%~fF" >> "%OUT%" 2>nul
     echo.>> "%OUT%"
 )
 
